b34b443e42322cb977d686aefd04d8795f6d0907
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2018-06-14 17:43:50 -0300

    Ultimo commit do dia. Aula 31 da seção 7.

---
+++ secao7/funcoes/exemplo-02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function salario(){
+             return 946.00;   
+            }
+            
+            echo "José recebeu 3 salários: " . (salario() * 3);
+        ?>
+    </body>
+</html>
