96f79d8a4d9b3774411aef19b629cccda4f55bc2
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2018-06-21 15:15:15 -0300

    Finalizando a Seção 12 com as aulas de 55 a 61.

---
+++ secao12/pdo/exemplo-02.php
@@ -0,0 +1,10 @@
+<?php
+
+$conn = new PDO("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "root", "");
+
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($results);
---
+++ secao12/pdo/exemplo-03.php
@@ -0,0 +1,13 @@
+<?php
+
+$conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
+
+$login = "José";
+$password = "1234567890";
+
+$stmt->bindParam(":LOGIN", $login);
+$stmt->bindParam(":PASSWORD", $password);
+
+$stmt->execute();
