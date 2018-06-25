05a15da11862069a06924b7d80dd7d403ab07308
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2018-06-25 17:04:37 -0300

    Commit da aula 64 da seção 13

--- DAO/index.php
+++ DAO/index.php
@@ -11,17 +11,30 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        
         require_once ("./config.php");
-        
+
 //        $sql = new Sql();
 //        $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //        
 //        echo json_encode($usuarios);
         
-            $root = new Usuario();
-            $root->loadById(3);
-            echo $root;
+        //Carrega um usuário
+        //$root = new Usuario();
+        //$root->loadById(3);
+        //echo $root;
+
+        //Carrega uma lista de usuários
+        //$lista = Usuario::getList();
+        //echo json_encode($lista);
+        
+        //Carrega uma lista de usuários buscando pelo Login
+        //$search = Usuario::search('jo');
+        //echo json_encode($search);
+        
+        //CARREGA UM USUÁRIO USANDO O LOGIN E A SENHA
+        $usuario = new Usuario();
+        $usuario->login("root", "!@#$%123");
+        echo $usuario
         ?>
     </body>
 </html>
