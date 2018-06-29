a778a325e0d3d40d2387f7df041ebad3e73fa265
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2018-06-29 09:43:48 -0300

    Finalizando Seção 20 com RainTPL. Aula 86!

---
+++ secao20/tpl/cache/index.6f6046c10913cbe82a0ec1b515423665.rtpl.php
@@ -0,0 +1,4 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1><?php echo htmlspecialchars( $name, ENT_COMPAT, 'UTF-8', FALSE ); ?> - Hello There</h1>
+<p><?php echo htmlspecialchars( $name2, ENT_COMPAT, 'UTF-8', FALSE ); ?> - General Kenobi!</p>
+
+<p>Versão do PHP: <?php echo htmlspecialchars( $version, ENT_COMPAT, 'UTF-8', FALSE ); ?></p>
